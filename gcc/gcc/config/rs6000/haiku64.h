8e697c230e4899f2791e17cd3d9a357bb4d14bd8
tsundoku <me@tsundoku.ne.jp>
2020-05-23 16:15:42 +0900

    Add initial 64-bit Haiku PowerPC Big Endian support.
    Change-Id: I64b4c8a1e3c92c635a66ea8ca41dad244da33d9a
    Reviewed-on: https://review.haiku-os.org/c/buildtools/+/2784
    Reviewed-by: Adrien Destugues <pulkomandy@pulkomandy.tk>

---
+++ gcc/gcc/config/rs6000/haiku64.h
@@ -0,0 +1,77 @@
+/* Definitions for 64-bit PowerPC running Haiku
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005
+   Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+
+/* long double is 128 bits wide; the documentation claims
+   LIBGCC2_LONG_DOUBLE_TYPE_SIZE to default to LONG_DOUBLE_TYPE_SIZE, but
+   it apparently does not */
+/*#undef LONG_DOUBLE_TYPE_SIZE
+#define LONG_DOUBLE_TYPE_SIZE 128
+#undef LIBGCC2_LONG_DOUBLE_TYPE_SIZE
+#define LIBGCC2_LONG_DOUBLE_TYPE_SIZE 128*/
+
+/* Undef gnu-user.h macros we don't want.  */
+#undef CPLUSPLUS_CPP_SPEC
+#undef LINK_GCC_C_SEQUENCE_SPEC
+
+#undef TARGET_DEFAULT
+#define TARGET_DEFAULT (MASK_POWERPC64|MASK_64BIT)
+
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()					\
+  do									\
+    {									\
+	builtin_define ("__HAIKU__");					\
+	builtin_define ("__POWERPC__");					\
+	builtin_define ("__powerpc__");					\
+	builtin_define ("__PPC64__");					\
+	builtin_define ("__powerpc64__");					\
+	builtin_define ("__STDC_ISO_10646__=201103L"); \
+	builtin_assert ("system=haiku");					\
+	builtin_assert ("cpu=powerpc64");					\
+	builtin_assert ("machine=powerpc64");					\
+    }									\
+  while (0)
+
+/* Provide a LINK_SPEC appropriate for Haiku.  Here we provide support
+   for the special GCC options -static and -shared, which allow us to
+   link things in one of these three modes by applying the appropriate
+   combinations of options at link-time.  */
+
+/* If ELF is the default format, we should not use /lib/elf.  */
+
+#undef	LINK_SPEC
+#define LINK_SPEC "-m elf64ppchaiku %{!r:-shared} %{nostart:-e 0} %{shared:-e 0} %{!shared: %{!nostart: -no-undefined}}"
+
+/* override options from sysv4.h */
+#undef SUBTARGET_OVERRIDE_OPTIONS
+#define SUBTARGET_OVERRIDE_OPTIONS                                    \
+ do {                                                                  \
+	rs6000_abi_name = "sysv";                                      \
+	rs6000_current_abi = ABI_ELFv2;                                   \
+	rs6000_sdata = SDATA_DATA;                                     \
+	rs6000_sdata_name = "data";                                    \
+	/*secure_plt = true;*/                                         \
+	rs6000_isa_flags |= OPTION_MASK_MINIMAL_TOC;                   \
+	TARGET_NO_FP_IN_TOC = 1;                                       \
+	haiku_override_options ();                                     \
+   }                                                               \
+ while (0)
